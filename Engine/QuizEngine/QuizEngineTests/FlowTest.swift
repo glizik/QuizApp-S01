ce827ae61f5b3f1470ee33f21dde3bca4e555218
Gabor Lizik <glizik@gmail.com>
2023-03-20 11:44:49 +0100

    start with the first question, check correct question, test duplicate call

--- Engine/QuizEngine/QuizEngineTests/FlowTest.swift
+++ Engine/QuizEngine/QuizEngineTests/FlowTest.swift
@@ -38,13 +38,44 @@ class FlowTest: XCTestCase {
         XCTAssertEqual(router.routedQuestion, "Q1")
     }
 
+    func test_start_withOneQuestion_routesToCorrectQuestion_2() {
+        let router = RouterSpy()
+        let sut = Flow(questions: ["Q2"], router: router)
+
+        sut.start()
+
+        XCTAssertEqual(router.routedQuestion, "Q2")
+    }
+
+    func test_start_withTwoQuestion_routesToFirstQuestion() {
+        let router = RouterSpy()
+        let sut = Flow(questions: ["Q1", "Q2"], router: router)
+
+        sut.start()
+
+        XCTAssertEqual(router.routedQuestion, "Q1")
+    }
+    
+    func test_startTwice_withTwoQuestion_routesToFirstQuestionTwice() {
+        let router = RouterSpy()
+        let sut = Flow(questions: ["Q1", "Q2"], router: router)
+
+        sut.start()
+        sut.start()
+        
+        XCTAssertEqual(router.routedQuestion, "Q1")
+    }
+    
+    
     class RouterSpy: Router {
         var routedQuestionCount: Int = 0
         var routedQuestion: String?
+        var routedQuestions: [String] = []
         
         func routeTo(question: String) {
             routedQuestionCount += 1
             routedQuestion = question
+            routedQuestions.append(question)
         }
     }
 }
